51ccb358b6fd0a343249f1fe89fb55095ff6622c
Daniel <dlozanoc862@gmail.com>
2023-04-28 19:32:46 -0500

    :sparkles: Creando encabezados dinamicos

--- src/components/ListaClientes.jsx
+++ src/components/ListaClientes.jsx
@@ -4,18 +4,32 @@ const ListaClientes = ({cliente}) => {
 
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
-            <h2 className="font-black text-2xl text-center">Listado de Citas</h2>
+
+        {cliente && cliente.length ? (
+            <>
+                <h2 className="font-black text-2xl text-center">Listado de Citas</h2>
                 <p className="text-lg mt-5 text-center">
                     Administra las {''}
                     <span className="text-fuchsia-600 font-bold">Citas del SPA</span>
                 </p>
-            
-            {cliente.map( (persona) =>(
-                <Cliente
-                    key={persona.id}
-                    persona={persona}
-                />
-            ))}
+                
+                {cliente.map( (persona) =>(
+                    <Cliente
+                        key={persona.id}
+                        persona={persona}
+                    />
+                ))}
+            </>
+        ): (
+            <>
+                <h2 className="font-black text-2xl text-center">No hay citas</h2>
+                <p className="text-lg mt-5 text-center">
+                    Comienza a agendar {''}
+                    <span className="text-fuchsia-600 font-bold">citas</span>
+                </p>
+            </>
+        )}
+
 
         </div>
     )
